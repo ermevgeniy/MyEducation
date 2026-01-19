cf9522967aaa1514294c7525a5c24e8f270de563
ErmEvgeniy <62184341+ErmEvgeniy@users.noreply.github.com>
2026-01-19 11:59:32 +0300

    string methods. tasks

--- STRINGI.py
+++ STRINGI.py
@@ -137,6 +137,172 @@ from itertools import count
 # print(s[::-1])
 # print(s[1:][:-1])
 
+# s = input()
+#
+# print(s[:3][-1:])
+# print(s[-2:][0:1])
+# print(s[:5])
+# print(s[:-2])
+# print(s[0::2])
+# print(s[1::2])
+# print(s[::-1])
+# print(s[::-2])
+
+
+# s = input()
+# l = len(s) + 1
+# part1 = s[0:l// 2]
+# part2 = s[l//2:]
+#
+# print(part2 + part1)
+
+# s = input()
+#
+# if s == s.title():
+#     print('YES')
+# else:
+#     print('NO')
+
+
+# s = input()
+# string = s.swapcase()
+# print(string)
+
+# print(input().swapcase())
+
+#
+# s = input()
+# a = 'хорош'
+#
+# if a in s.lower():
+#     print('YES')
+# else:
+#     print('NO')
+
+# s = input()
+# count = 0
+#
+# for i in range(len(s)):
+#     if  s[i].islower():
+#         count +=1
+# print(count)
+#
+# s = input()
+# count = 0
+#
+# for i in s:
+#     if i.lower() != i:
+#         count += 1
+#
+# print(count)
+
+# s = input()
+# print(s.count(' ') + 1)
+
+# s = input()
+# str = s.lower()
+# print('Аденин:', str.count('а'))
+# print('Гуанин:', str.count('г'))
+# print('Цитозин:', str.count('ц'))
+# print('Тимин:', str.count('т'))
+
+
+# n = int(input())
+# count = 0
+# for i in range(n):
+#     s = input()
+#     if s.count('11') > 2:
+#         count +=1
+#
+# print(count)
+
+# s = input()
+# count = 0
+#
+# for i in range(len(s)):
+#     if s[i] in '1234567890':
+#         count += 1
+# print(count)
+
+# s = input()
+#
+# if s.endswith('.com') or s.endswith('.ru'):
+#     print('YES')
+# else:
+#     print('NO')
+
+#
+# s = input()
+# counter = 0
+# counter1 = ''
+#
+# for i in s:
+#     if s.count(i) >= counter:
+#         counter = s.count(i)
+#         counter1 = i
+# print(counter1)
+
+#
+# s = input()
+# count = 0
+# for i in s:
+#     if i == 'f':
+#         count += 1
+# if count == 1:
+#     print(s.find('f'))
+#
+# if count > 1:
+#     print(s.find('f'), end=' ')
+#     print(s.rfind('f'))
+#
+# if count == 0:
+#     print('NO')
+
+# s= input()
+# if s.find('f') == -1:
+#     print('NO')
+# elif s.find('f') == s.rfind('f'):
+#     print(s.find('f'))
+# else:
+#     print(s.find('f'),s.rfind('f'))
+
+
+# s = input()
+#
+# for i in range(len(s)):
+#     if i == s.find('h'):
+#         print(s[:i], end='')
+#     if i == s.rfind('h'):
+#         print(s[i+1:])
+
+
+
+# n = int(input())
+#
+# for i in range(1, n + 1):
+#     comment = input()
+#     if len(comment) == 0:
+#         print(i, ':', ' ', 'COMMENT SHOULD BE DELETED', sep='')
+#     elif comment.isspace():
+#         print(i,':',' ', 'COMMENT SHOULD BE DELETED', sep='')
+#     else:
+#         print(i,':',' ', comment, sep='')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
