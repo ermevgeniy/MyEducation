6ca6e7c3e36ac0df0d42333cd9b5bb935642a4c0
ErmEvgeniy <62184341+ErmEvgeniy@users.noreply.github.com>
2026-02-03 17:41:07 +0300

    метод append()

--- LISTS.py
+++ LISTS.py
@@ -1,12 +1,33 @@
 from itertools import count
+#
+# n = input()
+# c = ''
+# numbers = list(n)
+#
+# for i in range(len(numbers)):
+#     if i % 2 == 0:
+#         c += numbers[i]
+# print(list(c))
 
-n = input()
-c = ''
-numbers = list(n)
+# rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+#
+# rainbow[3] = 'Зеленый'
+# rainbow[-1] = 'Фиолетовый'
+#
+# print(rainbow)
 
-for i in range(len(numbers)):
-    if i % 2 == 0:
-        c += numbers[i]
-print(list(c))
+# ---------------------------------
+# alf = 'abcdefghijklmnopqrstuvwxyz'
+# count = ['a']
+# for i in range(1, len(alf)):
+#     count.append(alf[i] * (i + 1))
+#
+# print(count)
 
-
+# ---------------------------------
+# n = int(input())
+# lst = []
+# for i in range(n):
+#     str = input()
+#     lst.append(str)
+# print(lst)
