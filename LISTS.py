9e8eaf92da64d307366091ccab65062ebf6f8a43
ErmEvgeniy <62184341+ErmEvgeniy@users.noreply.github.com>
2026-02-04 13:47:19 +0300

    методы extend(), del()

--- LISTS.py
+++ LISTS.py
@@ -31,3 +31,84 @@ from itertools import count
 #     str = input()
 #     lst.append(str)
 # print(lst)
+
+# -----------------------------
+
+# numbers = [2, 6, 3, 14, 10, 4, 11, 16, 12, 5, 4, 16, 1, 0, 8, 16, 10, 10, 8, 5, 1, 11,
+#            10, 10, 12, 0, 0, 6, 14, 8, 2, 12, 14, 5, 6, 12, 1, 2, 10, 14, 9, 1, 15, 1, 2, 14, 16, 6, 7, 5]
+#
+# print(len(numbers))
+# print(numbers[-1])
+# print(numbers[::-1])
+# if 5 in numbers and 17 in numbers:
+#     print('YES')
+# else:
+#     print('NO')
+# del numbers[0]
+# del numbers[-1]
+# print(numbers)
+
+# ----------------
+
+# n = int(input())
+# lst = []
+# for i in range(1, n + 1):
+#     if n % i == 0:
+#         lst.append(i)
+# print(lst)
+
+# -----------------
+
+# n = int(input())
+# lst = []
+# numbers = int(input())
+# for i in range(n-1):
+#     count = numbers
+#     numbers = int(input())
+#     lst.append(numbers + count)
+# print(lst)
+
+# -------------------
+
+# n = int(input())
+# lst = []
+# for i in range(n):
+#     number = int(input())
+#     lst.append(number)
+# del lst[1::2]
+# print(lst)
+
+# -------------------
+
+# n = int(input())
+# lst = []
+# s = ''
+# s1 = ''
+# for i in range(n):
+#      str = input()
+#      lst.append(str)
+# k = int(input())
+# for h in range(len(lst)):
+#      s = lst[h]
+#      if k <= len(s):
+#         s1 += s[k-1]
+#      else:
+#          continue
+# print(s1)
+
+# -----------------------
+
+# n = int(input())
+# lst = []
+# for _ in range(n):
+#     s = input()
+#     lst.extend(s)
+# print(lst)
+
+# --------------------
+
+
+
+
+
+
