b5a598b159d8023d8a601bc3bd247023cc515dbf
ErmEvgeniy <62184341+ErmEvgeniy@users.noreply.github.com>
2026-02-16 15:29:17 +0300

    Списочные выражения

--- LISTS.py
+++ LISTS.py
@@ -1,4 +1,6 @@
-from itertools import count
+from itertools import count, chain
+from re import split
+
 #
 # n = input()
 # c = ''
@@ -284,8 +286,107 @@ from itertools import count
 
 # --------------------
 
+# num_str = input().split()
+#
+# int_num = []
+#
+# for i in range(len(num_str)):
+#     int_num.append(int(num_str[i]))
+#
+# int_num.sort()
+# print(*int_num)
+# int_num.sort(reverse=True)
+# print(*int_num)
+
+# --------------------
+
+# n = int(input())
+# songs = []
+#
+# for i in range(n):
+#     song = input()
+#     songs.append(song)
+#
+# songs.sort()
+# print(*songs, sep='\n')
+# print('\n'.join(songs))
+
+# --------------------
 
 
+# string = (input())
+# string.split()
+# n = int(string[1:])
+#
+# counter = []
+# for i in range(n):
+#     text = input()
+#     if '#' in text:
+#         text = text[0: text.find('#')]
+#     text = text.rstrip()
+#     counter.append(text)
+#
+# print(*counter, sep='\n')
+
+# --------------------
+
+
+# keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class',
+#             'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for',
+#             'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or',
+#             'pass', 'raise', 'return', 'while', 'yield']
+#
+# lengths = [i for i in keywords if len(i) >= 5]
+#
+# print(lengths)
+
+# --------------------
+
+# palindromes = [i for i in range(100, 1001) if str(i) == str(i)[::-1]]
+#
+# print(palindromes)
+
+# --------------------
+
+# squere = [i ** 2 for i in range(1, int(input())+1)]
+#
+# print(*squere, sep='\n')
+
+
+# --------------------
+
+# stringi = input().split()
+# c = []
+# for i in stringi:
+#     c.append(int(i) ** 3)
+#
+# print(*c)
+#  то же через списочное выражение
+
+# cube =  [int(i) ** 3 for i in input().split()]
+#
+# print(*cube)
+
+# --------------------
+
+# s = input()
+# c = []
+# for i in s:
+#     if i in '0123456789':
+#         c.append(i)
+#
+# print(*c, sep='')
+
+# то же через списочное выражение
+# number_only = [ i for i in input() if i in '0123456789']
+#
+# print(*number_only, sep='')
+
+# --------------------
+
+# quare = [ int(i) ** 2 for i in (input().split()) if int(i) % 2 == 0 and (int(i)**2) % 10 != 4]
+#
+# print(*quare)
 
 
 
